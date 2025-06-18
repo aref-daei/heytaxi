78dc4070c699031fd457c4eb1266c83665170d8c
Aref Daei <aref.daei@outlook.com>
2025-06-18 20:36:24 +0330

    Added Menu

--- src/application/SystemManagement.java
+++ src/application/SystemManagement.java
@@ -6,28 +6,81 @@ import java.util.Scanner;
 
 public class SystemManagement {
 	private static List<Driver> drivers = randomDriverGenerator();
+	private static Traveler session = new Traveler("Unknown");
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Please entire your name:");
-		String name = input.nextLine();
-		System.out.println("Please entire your location:");
-		int xTraveler = input.nextInt();
-		int yTraveler = input.nextInt();
-		System.out.println("Please entire your location destination:");
-		int xDestination = input.nextInt();
-		int yDestination = input.nextInt();
-		input.nextLine();
-
+		// ** Menu **
+		boolean menu = true;
+		while (menu) {
+			System.out.println("Welcome to HeyTaxi!");
+			
+			System.out.println();
+			System.out.println("1. Sign in");
+			System.out.println("2. History");
+			System.out.println("3. Quit");
+			System.out.println();
+			
+			System.out.print("Choice option: ");
+			String opt = input.nextLine();
+			
+			switch (opt.charAt(0)) {
+			case '1': {
+				boolean loop = true;
+				while (loop) {
+					System.out.println("Please enter your full name.");
+					session.setName(input.nextLine());;
+					System.out.println("Are you really yourself? y/n");
+					String isYourself = input.nextLine();
+					
+					if (isYourself.charAt(0) != 'y') {
+						System.out.printf("You are not %s. Please try again.%n", session.getName());
+					} else {
+						loop = false;
+					}
+				}
+				
+				System.out.println("Do you know your location? y/n\nIf \"no\", your location (0, 0) will be saved.");
+				String hasLocation = input.nextLine();
+				
+				if (hasLocation.charAt(0) == 'y') {
+					System.out.println("Please enter your location as sample. ex: 5 13");
+					session.setX(input.nextInt());
+					session.setY(input.nextInt());
+				}		
+				
+				System.out.println("Please enter your location destination as sample. ex: 6 -2");
+				int xDestination = input.nextInt();
+				int yDestination = input.nextInt();
+				
+				
+				Travel travel = new Travel(drivers.get(2), session, new int[]{xDestination, yDestination});
+				System.out.printf(
+						"Traveler: %s%nDriver: %s%nTravel Date: %s%nDistance: %.2fKM%nTime: %dmin%nCost: %d%n",
+						travel.getTraveler().getName(), travel.getDriver(),
+						"Jun 18", travel.getDistance(), travel.getTime(), travel.getCost());
+				
+				break;
+			}
+			case '2': {
+//				for (Travel travel : travels) {
+//					System.out.println(travel);
+//				}
+				
+				break;
+			}
+			case '3':
+				return;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opt.charAt(0));
+			}
+			
+			System.out.println("Agian? true/false");
+			menu = input.nextBoolean();
+		}
+		
 		input.close();
-		
-		Traveler traveler = new Traveler(name);
-		traveler.setX(xTraveler);
-		traveler.setY(yTraveler);
-		
-		Travel travel = new Travel(drivers.get(2), traveler, new int[]{xDestination, yDestination});
-		System.out.println(travel.getTime());
 		
 //		int porpus = (int)(Math.random()*(500-5+1)+5) * (int)Math.pow(-1,(int)(Math.random()*2));
 	}
