246d3d37b4839a1fe2842afe8422cfc23ab5822a
Aref Daei <aref.daei@outlook.com>
2025-06-21 19:34:30 +0330

    Added Phone field to Traveler class

--- src/application/Traveler.java
+++ src/application/Traveler.java
@@ -1,14 +1,28 @@
 package application;
 
 public class Traveler extends Personal {
-	public Traveler(String name) {
+	private String phone;
+	
+	public Traveler(String name, String phone) {
 		super(name, 5);
+		
+		this.phone = phone;
 	}
 	
-	public Traveler(String name, double score) {
+	public Traveler(String name, String phone, double score) {
 		super(name, score);
+		
+		this.phone = phone;
 	}
 	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 	@Override
 	public String toString() {
 		return String.format("%s: %s - %.2f Score",
