e683c63380a2e4e5f7da73b4584c573c90757c7a
Aref Daei <aref.daei@outlook.com>
2025-07-29 14:34:36 +0330

    Create Screen class to clear terminal screen

---
+++ src/main/java/ir/ardastudio/shared/Screen.java
@@ -0,0 +1,19 @@
+package ir.ardastudio.shared;
+
+import java.io.IOException;
+
+public class Screen {
+	// This method for clear terminal screen
+	public static void clear() {
+        try {
+            String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
