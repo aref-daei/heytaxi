788d3b5c5759c367e8da9beb787c2f38f99c965e
Aref Daei <aref.daei@outlook.com>
2025-08-09 18:14:22 +0330

    Renamed Main to Application

--- src/main/java/ir/ardastudio/Application.java
+++ src/main/java/ir/ardastudio/Application.java
@@ -1,20 +1,9 @@
 package ir.ardastudio;
 
 import ir.ardastudio.app.SystemManagement;
-import java.sql.SQLException;
 
 public class Application {
     public static void main(String[] args) {
         SystemManagement.launch();
-//        try {
-//            SystemManagement.launch();
-//        } catch (SQLException e) {
-//            System.err.println("Database connection failed: " + e.getMessage());
-//            System.exit(1);
-//        } catch (Exception e) {
-//            System.err.println("Application error: " + e.getMessage());
-//            e.printStackTrace();
-//            System.exit(1);
-//        }
     }
 }
--- src/main/java/ir/ardastudio/Main.java
+++
@@ -1,9 +0,0 @@
-package ir.ardastudio;
-
-import ir.ardastudio.app.SystemManagement;
-
-public class Main {
-    public static void main(String[] args) {
-        SystemManagement.launch();
-    }
-}
