aa10b30a7ea4ef2b0a01b9c5186fdfd5727d8bc3
Aref Daei <aref.daei@outlook.com>
2025-08-09 18:10:43 +0330

    Created DriverRepository

---
+++ src/main/java/ir/ardastudio/repositories/DriverRepository.java
@@ -0,0 +1,94 @@
+package ir.ardastudio.repositories;
+
+import ir.ardastudio.models.Car;
+import ir.ardastudio.models.Driver;
+import ir.ardastudio.utils.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DriverRepository {
+    public void addDriver(Driver driver) throws SQLException {
+        String personSQL = "INSERT INTO person VALUES(?, ?, ?, ?, ?)";
+        String driverSQL = "INSERT INTO driver VALUES(?, ?)";
+
+        try (Connection connection = DBConnection.getConnection()) {
+            // Person
+            PreparedStatement personStmt = connection.prepareStatement(personSQL);
+            personStmt.setInt(1, driver.getId());
+            personStmt.setString(2, driver.getName());
+            personStmt.setInt(3, driver.getX());
+            personStmt.setInt(4, driver.getY());
+            personStmt.setDouble(5, driver.getScore());
+            personStmt.executeUpdate();
+
+            // Driver
+            PreparedStatement driverStmt = connection.prepareStatement(driverSQL);
+            driverStmt.setInt(1, driver.getId());
+            driverStmt.setInt(2, driver.getCar().getId());
+            driverStmt.executeUpdate();
+        }
+    }
+
+    public List<Driver> getAllDrivers() throws SQLException {
+        List<Driver> drivers = new ArrayList<>();
+        String sql = "SELECT * FROM person JOIN driver d ON person.id = d.id JOIN car ON d.car_id = car.id";
+
+        try (Connection connection = DBConnection.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                Car car = new Car(
+                        rs.getString("car.model"),
+                        rs.getString("car.color"),
+                        rs.getString("car.licensePlate"));
+                car.setId(rs.getInt("car.id"));
+
+                Driver driver = new Driver(
+                        rs.getString("name"),
+                        rs.getDouble("score"),
+                        car);
+                driver.setId(rs.getInt("person.id"));
+                driver.setX(rs.getInt("x"));
+                driver.setY(rs.getInt("y"));
+
+                drivers.add(driver);
+            }
+        }
+
+        return drivers;
+    }
+
+    public Driver getDriverById(int id) throws SQLException {
+        String sql = "SELECT * FROM person JOIN driver d ON person.id = d.id JOIN car ON d.car_id = car.id WHERE person.id = ?";
+
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preStatement = connection.prepareStatement(sql);
+
+            preStatement.setInt(1, id);
+            ResultSet rs = preStatement.executeQuery();
+
+            if (rs.next()) {
+                Car car = new Car(
+                        rs.getString("car.model"),
+                        rs.getString("car.color"),
+                        rs.getString("car.licensePlate"));
+                car.setId(rs.getInt("car.id"));
+
+                Driver driver = new Driver(
+                        rs.getString("name"),
+                        rs.getDouble("score"),
+                        car);
+                driver.setId(rs.getInt("person.id"));
+                driver.setX(rs.getInt("x"));
+                driver.setY(rs.getInt("y"));
+
+                return driver;
+            }
+        }
+
+        return null;
+    }
+}
