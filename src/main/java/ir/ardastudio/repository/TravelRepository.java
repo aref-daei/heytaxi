795d43a54512ead70360a0ed0b11a2603deff56a
Aref Daei <aref.daei@outlook.com>
2025-08-12 01:38:52 +0330

    Update TravelRepository

--- src/main/java/ir/ardastudio/repository/TravelRepository.java
+++ src/main/java/ir/ardastudio/repository/TravelRepository.java
@@ -12,8 +12,10 @@ public class TravelRepository {
     public void addTravel(Travel travel) throws SQLException {
         String sql = "INSERT INTO travel VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try (Connection connection = DBConnection.getConnection()) {
-            PreparedStatement prepStmt = connection.prepareStatement(sql);
+        try (
+                Connection connection = DBConnection.getConnection();
+                PreparedStatement prepStmt = connection.prepareStatement(sql)
+        ) {
             prepStmt.setInt(1, travel.getId());
             prepStmt.setInt(2, travel.getDriver().getId());
             prepStmt.setInt(3, travel.getTraveler().getId());
@@ -31,16 +33,17 @@ public class TravelRepository {
     public List<Travel> getAllTravels() throws SQLException {
         List<Travel> travels = new ArrayList<>();
         String sql = "SELECT * FROM travel t" +
-                "JOIN person pd ON pd.id = t.driver_id" +
-                "JOIN driver ON driver.id = person.id" +
-                "JOIN person pt ON pt.id = t.traveler_id" +
-                "JOIN traveler ON traveler.id = person.id" +
+                "JOIN person pd ON t.driver_id = pd.id" +
+                "JOIN driver ON pd.id = driver.id" +
+                "JOIN person pt ON t.traveler_id = pt.id" +
+                "JOIN traveler ON pt.id = traveler.id" +
                 "JOIN car ON car.id = driver.car_id";
 
-        try (Connection connection = DBConnection.getConnection()) {
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(sql);
-
+        try (
+                Connection connection = DBConnection.getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery(sql)
+        ) {
             while (rs.next()) {
                 Car car = new Car(
                         rs.getString("car.model"),
@@ -49,24 +52,23 @@ public class TravelRepository {
                 car.setId(rs.getInt("car.id"));
 
                 Driver driver = new Driver(
-                        rs.getString("driver.name"),
-                        rs.getDouble("driver.score"),
+                        rs.getString("pd.name"),
+                        rs.getDouble("pd.score"),
                         car);
                 driver.setId(rs.getInt("pd.id"));
                 driver.setX(rs.getInt("pd.x"));
                 driver.setY(rs.getInt("pd.y"));
 
                 Traveler traveler = new Traveler(
-                        rs.getString("traveler.name"),
-                        rs.getString("traveler.phone"),
-                        rs.getDouble("traveler.score"));
+                        rs.getString("pt.name"),
+                        rs.getString("pt.phone"),
+                        rs.getDouble("pt.score"));
                 traveler.setId(rs.getInt("pt.id"));
                 traveler.setX(rs.getInt("pt.x"));
                 traveler.setY(rs.getInt("pt.y"));
 
                 Travel travel = new Travel(
-                        driver,
-                        traveler,
+                        driver, traveler,
                         new int[]{rs.getInt("t.dest_x"), rs.getInt("t.dest_y")});
                 travel.setId(rs.getInt("t.id"));
                 travel.setDate(rs.getString("t.date"));
@@ -77,62 +79,60 @@ public class TravelRepository {
                 travels.add(travel);
             }
         }
-
         return travels;
     }
 
     public Travel getTravelById(int id) throws SQLException {
         String sql = "SELECT * FROM travel t" +
-                "JOIN person pd ON pd.id = t.driver_id" +
-                "JOIN driver ON driver.id = person.id" +
-                "JOIN person pt ON pt.id = t.traveler_id" +
-                "JOIN traveler ON traveler.id = person.id" +
+                "JOIN person pd ON t.driver_id = pd.id" +
+                "JOIN driver ON pd.id = driver.id" +
+                "JOIN person pt ON t.traveler_id = pt.id" +
+                "JOIN traveler ON pt.id = traveler.id" +
                 "JOIN car ON car.id = driver.car_id" +
                 "WHERE t.id = ?";
 
-        try (Connection connection = DBConnection.getConnection()) {
-            PreparedStatement preStatement = connection.prepareStatement(sql);
-
+        try (
+                Connection connection = DBConnection.getConnection();
+                PreparedStatement preStatement = connection.prepareStatement(sql)
+        ) {
             preStatement.setInt(1, id);
-            ResultSet rs = preStatement.executeQuery();
+            try (ResultSet rs = preStatement.executeQuery()) {
+                if (rs.next()) {
+                    Car car = new Car(
+                            rs.getString("car.model"),
+                            rs.getString("car.color"),
+                            rs.getString("car.licensePlate"));
+                    car.setId(rs.getInt("car.id"));
 
-            if (rs.next()) {
-                Car car = new Car(
-                        rs.getString("car.model"),
-                        rs.getString("car.color"),
-                        rs.getString("car.licensePlate"));
-                car.setId(rs.getInt("car.id"));
+                    Driver driver = new Driver(
+                            rs.getString("pd.name"),
+                            rs.getDouble("pd.score"),
+                            car);
+                    driver.setId(rs.getInt("pd.id"));
+                    driver.setX(rs.getInt("pd.x"));
+                    driver.setY(rs.getInt("pd.y"));
 
-                Driver driver = new Driver(
-                        rs.getString("driver.name"),
-                        rs.getDouble("driver.score"),
-                        car);
-                driver.setId(rs.getInt("pd.id"));
-                driver.setX(rs.getInt("pd.x"));
-                driver.setY(rs.getInt("pd.y"));
+                    Traveler traveler = new Traveler(
+                            rs.getString("pt.name"),
+                            rs.getString("pt.phone"),
+                            rs.getDouble("pt.score"));
+                    traveler.setId(rs.getInt("pt.id"));
+                    traveler.setX(rs.getInt("pt.x"));
+                    traveler.setY(rs.getInt("pt.y"));
 
-                Traveler traveler = new Traveler(
-                        rs.getString("traveler.name"),
-                        rs.getString("traveler.phone"),
-                        rs.getDouble("traveler.score"));
-                traveler.setId(rs.getInt("pt.id"));
-                traveler.setX(rs.getInt("pt.x"));
-                traveler.setY(rs.getInt("pt.y"));
+                    Travel travel = new Travel(
+                            driver, traveler,
+                            new int[]{rs.getInt("t.dest_x"), rs.getInt("t.dest_y")});
+                    travel.setId(rs.getInt("t.id"));
+                    travel.setDate(rs.getString("t.date"));
+                    travel.setDistance(rs.getDouble("t.distance"));
+                    travel.setTime(rs.getInt("t.time"));
+                    travel.setStatus(rs.getString("t.status"));
 
-                Travel travel = new Travel(
-                        driver,
-                        traveler,
-                        new int[]{rs.getInt("t.dest_x"), rs.getInt("t.dest_y")});
-                travel.setId(rs.getInt("t.id"));
-                travel.setDate(rs.getString("t.date"));
-                travel.setDistance(rs.getDouble("t.distance"));
-                travel.setTime(rs.getInt("t.time"));
-                travel.setStatus(rs.getString("t.status"));
-
-                return travel;
+                    return travel;
+                }
             }
         }
-
         return null;
     }
 }
