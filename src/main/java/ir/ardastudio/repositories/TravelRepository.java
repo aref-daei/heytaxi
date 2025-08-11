0a881edd8960df526030bcbae762aac227d2afa5
Aref Daei <aref.daei@outlook.com>
2025-08-11 13:43:08 +0330

    Completed TravelRepository

--- src/main/java/ir/ardastudio/repositories/TravelRepository.java
+++ src/main/java/ir/ardastudio/repositories/TravelRepository.java
@@ -1,6 +1,7 @@
 package ir.ardastudio.repositories;
 
-import ir.ardastudio.models.Travel;
+import ir.ardastudio.models.*;
+import ir.ardastudio.models.Driver;
 import ir.ardastudio.utils.DBConnection;
 
 import java.sql.*;
@@ -27,50 +28,111 @@ public class TravelRepository {
         }
     }
 
-//    public List<Travel> getAllTravels() throws SQLException {
-//        List<Travel> travels = new ArrayList<>();
-//        String sql = "SELECT * FROM travel";
-//
-//        try (Connection connection = DBConnection.getConnection()) {
-//            Statement statement = connection.createStatement();
-//            ResultSet rs = statement.executeQuery(sql);
-//
-//            while (rs.next()) {
-//                Travel travel = new Travel(
-//                        rs.getString("name"),
-//                        rs.getString("phone"),
-//                        rs.getDouble("score"));
-//                travel.setId(rs.getInt("person.id"));
-//                travel.setX(rs.getInt("x"));
-//                travel.setY(rs.getInt("y"));
-//                travels.add(travel);
-//            }
-//        }
-//
-//        return travels;
-//    }
-//
-//    public Travel getTravelById(int id) throws SQLException {
-//        String sql = "SELECT * FROM travel";
-//
-//        try (Connection connection = DBConnection.getConnection()) {
-//            PreparedStatement preStatement = connection.prepareStatement(sql);
-//
-//            preStatement.setInt(1, id);
-//            ResultSet rs = preStatement.executeQuery();
-//
-//            if (rs.next()) {
-//                Travel travel = new Travel(
-//                        rs.getString("name"),
-//                        rs.getString("phone"),
-//                        rs.getDouble("score"));
-//                travel.setId(rs.getInt("person.id"));
-//                travel.setX(rs.getInt("x"));
-//                travel.setY(rs.getInt("y"));
-//                return travel;
-//            }
-//        }
-//
-//        return null;
-//    }
+    public List<Travel> getAllTravels() throws SQLException {
+        List<Travel> travels = new ArrayList<>();
+        String sql = "SELECT * FROM travel t" +
+                "JOIN person pd ON pd.id = t.driver_id" +
+                "JOIN driver ON driver.id = person.id" +
+                "JOIN person pt ON pt.id = t.traveler_id" +
+                "JOIN traveler ON traveler.id = person.id" +
+                "JOIN car ON car.id = driver.car_id";
+
+        try (Connection connection = DBConnection.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                Car car = new Car(
+                        rs.getString("car.model"),
+                        rs.getString("car.color"),
+                        rs.getString("car.licensePlate"));
+                car.setId(rs.getInt("car.id"));
+
+                Driver driver = new Driver(
+                        rs.getString("driver.name"),
+                        rs.getDouble("driver.score"),
+                        car);
+                driver.setId(rs.getInt("pd.id"));
+                driver.setX(rs.getInt("pd.x"));
+                driver.setY(rs.getInt("pd.y"));
+
+                Traveler traveler = new Traveler(
+                        rs.getString("traveler.name"),
+                        rs.getString("traveler.phone"),
+                        rs.getDouble("traveler.score"));
+                traveler.setId(rs.getInt("pt.id"));
+                traveler.setX(rs.getInt("pt.x"));
+                traveler.setY(rs.getInt("pt.y"));
+
+                Travel travel = new Travel(
+                        driver,
+                        traveler,
+                        new int[]{rs.getInt("t.dest_x"), rs.getInt("t.dest_y")});
+                travel.setId(rs.getInt("t.id"));
+                travel.setDate(rs.getString("t.date"));
+                travel.setDistance(rs.getDouble("t.distance"));
+                travel.setTime(rs.getInt("t.time"));
+                travel.setStatus(rs.getString("t.status"));
+
+                travels.add(travel);
+            }
+        }
+
+        return travels;
+    }
+
+    public Travel getTravelById(int id) throws SQLException {
+        String sql = "SELECT * FROM travel t" +
+                "JOIN person pd ON pd.id = t.driver_id" +
+                "JOIN driver ON driver.id = person.id" +
+                "JOIN person pt ON pt.id = t.traveler_id" +
+                "JOIN traveler ON traveler.id = person.id" +
+                "JOIN car ON car.id = driver.car_id" +
+                "WHERE t.id = ?";
+
+        try (Connection connection = DBConnection.getConnection()) {
+            PreparedStatement preStatement = connection.prepareStatement(sql);
+
+            preStatement.setInt(1, id);
+            ResultSet rs = preStatement.executeQuery();
+
+            if (rs.next()) {
+                Car car = new Car(
+                        rs.getString("car.model"),
+                        rs.getString("car.color"),
+                        rs.getString("car.licensePlate"));
+                car.setId(rs.getInt("car.id"));
+
+                Driver driver = new Driver(
+                        rs.getString("driver.name"),
+                        rs.getDouble("driver.score"),
+                        car);
+                driver.setId(rs.getInt("pd.id"));
+                driver.setX(rs.getInt("pd.x"));
+                driver.setY(rs.getInt("pd.y"));
+
+                Traveler traveler = new Traveler(
+                        rs.getString("traveler.name"),
+                        rs.getString("traveler.phone"),
+                        rs.getDouble("traveler.score"));
+                traveler.setId(rs.getInt("pt.id"));
+                traveler.setX(rs.getInt("pt.x"));
+                traveler.setY(rs.getInt("pt.y"));
+
+                Travel travel = new Travel(
+                        driver,
+                        traveler,
+                        new int[]{rs.getInt("t.dest_x"), rs.getInt("t.dest_y")});
+                travel.setId(rs.getInt("t.id"));
+                travel.setDate(rs.getString("t.date"));
+                travel.setDistance(rs.getDouble("t.distance"));
+                travel.setTime(rs.getInt("t.time"));
+                travel.setStatus(rs.getString("t.status"));
+
+                return travel;
+            }
+        }
+
+        return null;
+    }
 }
