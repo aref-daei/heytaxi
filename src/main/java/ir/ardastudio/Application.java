9d12f88d3d04fde5727c035416c6bb6875769fd4
Aref Daei <aref.daei@outlook.com>
2025-08-11 15:50:11 +0330

    Updated Application

--- src/main/java/ir/ardastudio/Application.java
+++ src/main/java/ir/ardastudio/Application.java
@@ -1,9 +1,19 @@
 package ir.ardastudio;
 
 import ir.ardastudio.app.SystemManagement;
+import ir.ardastudio.utils.DBConnection;
+
+import java.sql.SQLException;
 
 public class Application {
     public static void main(String[] args) {
+        try {
+            DBConnection.initialize();
+        } catch (SQLException e) {
+            System.err.println("Error connecting Database: " + e.getMessage());
+            return;
+        }
+
         SystemManagement.launch();
     }
 }
