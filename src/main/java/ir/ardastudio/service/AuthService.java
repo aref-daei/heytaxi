884cce7e12f509bcdc775f32ca3bf143c2e9ea9a
Aref Daei <aref.daei@outlook.com>
2025-08-14 19:56:33 +0330

    Updated AuthService

--- src/main/java/ir/ardastudio/service/AuthService.java
+++ src/main/java/ir/ardastudio/service/AuthService.java
@@ -1,49 +1,64 @@
 package ir.ardastudio.service;
 
 import ir.ardastudio.model.Traveler;
-import ir.ardastudio.shared.SMSAuthentication;
-import ir.ardastudio.shared.Screen;
-import ir.ardastudio.shared.Telephone;
+import ir.ardastudio.repository.TravelerRepository;
+import ir.ardastudio.shared.*;
+import ir.ardastudio.util.IdGenerator;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AuthService {
-    public Traveler signIn(Scanner input) {
-        Screen.clear();
-        SMSAuthentication auth = new SMSAuthentication("logs.csv");
-        String name, phone;
+    private final TravelerRepository travelerRepo = new TravelerRepository();
+    private final SMSAuthentication auth = new SMSAuthentication("logs.csv");
 
-        while (true) {
-            try {
-                System.out.println("Please enter your phone number:");
-                phone = Telephone.corrector(input.nextLine());
-            } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
-                continue;
+    private Traveler traveler;
+
+    public Traveler getTraveler() {
+        return traveler;
+    }
+
+    public void signIn(Scanner input) {
+        try {
+            Screen.clear();
+            String name, phone;
+
+            while (true) {
+                try {
+                    System.out.println("Please enter your phone number:");
+                    phone = Telephone.corrector(input.nextLine());
+                } catch (IllegalArgumentException e) {
+                    System.err.println(e.getMessage());
+                    continue;
+                }
+
+                System.out.printf("Please enter the code sent to your mobile phone.%n%s%n",
+                        auth.sendAuthenticationSMS(phone));
+                String code = input.nextLine();
+                if (!auth.verifyCode(code, phone)) {
+                    System.err.println("Invalid code! Please try again.");
+                    continue;
+                }
+
+                System.out.println("Code confirmed.\nPlease enter your full name:");
+                name = input.nextLine();
+
+                System.out.println("Do you confirm the information entered is correct? Y/n");
+                String confirm = input.nextLine().toLowerCase();
+                if (confirm.isEmpty() || confirm.charAt(0) == 'y') {
+                    break;
+                }
             }
 
-            System.out.printf("Please enter the code sent to your mobile phone.%n%s%n",
-                    auth.sendAuthenticationSMS(phone));
-            String code = input.nextLine();
-            if (!auth.verifyCode(code, phone)) {
-                System.err.println("Invalid code! Please try again.");
-                continue;
-            }
-
-            System.out.println("Code confirmed.\nPlease enter your full name:");
-            name = input.nextLine();
-
-            System.out.println("Do you confirm the information entered is correct? Y/n");
-            String confirm = input.nextLine().toLowerCase();
-            if (confirm.isEmpty() || confirm.charAt(0) == 'y') {
-                break;
-            }
+            traveler = new Traveler(IdGenerator.generate(), name, phone);
+            travelerRepo.addTraveler(traveler);
+        } catch (SQLException e) {
+            System.err.println("Error fetching travelers: " + e.getMessage());
         }
-
-        return new Traveler(name, phone);
     }
 
     public void logOut() {
+        traveler = null;
         Screen.clear();
         System.out.println("You have been logged out.");
     }
