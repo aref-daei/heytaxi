b23f96ad100f61129f06f5f4175a573f3b9e04e3
Aref Daei <aref.daei@outlook.com>
2025-09-07 13:52:49 +0330

    Create Model class

---
+++ src/main/java/ir/ardastudio/model/Model.java
@@ -0,0 +1,52 @@
+package ir.ardastudio.model;
+
+import ir.ardastudio.util.IdGenerator;
+
+import java.time.LocalDateTime;
+
+public abstract class Model {
+    private String id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    protected void onId() {
+        this.id = IdGenerator.generate();
+    }
+
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Model that)) {
+            return false;
+        }
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
