c9db88cb4fd5efd8f645e2bead8c92edb667241e
Aref Daei <aref.daei@outlook.com>
2025-09-12 18:28:23 +0330

    Update DriverFinderService to match new Model structure

--- src/main/java/ir/ardastudio/service/DriverFinderService.java
+++ src/main/java/ir/ardastudio/service/DriverFinderService.java
@@ -1,7 +1,7 @@
 package ir.ardastudio.service;
 
 import ir.ardastudio.model.Driver;
-import ir.ardastudio.model.Traveler;
+import ir.ardastudio.model.User;
 import ir.ardastudio.repository.DriverRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +16,7 @@ public class DriverFinderService {
 
     private static final int MAX_ACTIVITY_RADIUS = 20;
 
-    public Driver findNearestDriver(Traveler traveler) {
+    public Driver findNearestDriver(User user) {
         try {
             List<Driver> drivers = driverRepo.getAllDrivers();
 
@@ -28,7 +28,7 @@ public class DriverFinderService {
             double minDistance = MAX_ACTIVITY_RADIUS + 1;
 
             for (Driver driver : drivers) {
-                double distance = calculateDistance(driver.getX(), driver.getY(), traveler.getX(), traveler.getY());
+                double distance = calculateDistance(driver.getX(), driver.getY(), user.getX(), user.getY());
                 if (distance < minDistance) {
                     minDistance = distance;
                     nearest = driver;
@@ -43,7 +43,7 @@ public class DriverFinderService {
         return null;
     }
 
-    private double calculateDistance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    private static double calculateDistance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
 }
