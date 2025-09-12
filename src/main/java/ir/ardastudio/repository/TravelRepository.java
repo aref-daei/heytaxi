ff00bba0a1e7aa79433d11ae8cecd8a69762a976
Aref Daei <aref.daei@outlook.com>
2025-09-12 18:08:26 +0330

    Update TravelRepository to match new Model structure

--- src/main/java/ir/ardastudio/repository/TravelRepository.java
+++ src/main/java/ir/ardastudio/repository/TravelRepository.java
@@ -10,22 +10,23 @@ import java.util.List;
 
 public class TravelRepository {
     public void addTravel(Travel travel) throws SQLException {
-        String sql = "INSERT INTO travel VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO travel VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try (
                 Connection connection = DBConnection.getConnection();
                 PreparedStatement prepStmt = connection.prepareStatement(sql)
         ) {
             prepStmt.setString(1, travel.getId());
-            prepStmt.setString(2, travel.getDriver().getId());
-            prepStmt.setString(3, travel.getTraveler().getId());
-            prepStmt.setString(4, travel.getDate());
-            prepStmt.setInt(5, travel.getDestination()[0]);
-            prepStmt.setInt(6, travel.getDestination()[1]);
-            prepStmt.setDouble(7, travel.getDistance());
-            prepStmt.setInt(8, travel.getTime());
-            prepStmt.setInt(9, travel.getCost());
-            prepStmt.setString(10, travel.getStatus());
+            prepStmt.setString(2, travel.getCreatedAt().toString());
+            prepStmt.setString(3, travel.getUpdatedAt().toString());
+            prepStmt.setString(4, travel.getDriver().getId());
+            prepStmt.setString(5, travel.getUser().getId());
+            prepStmt.setInt(6, travel.getX());
+            prepStmt.setInt(7, travel.getY());
+            prepStmt.setDouble(8, travel.getDistance());
+            prepStmt.setInt(9, travel.getTime());
+            prepStmt.setInt(10, travel.getCost());
+            prepStmt.setString(11, travel.getStatus());
             prepStmt.executeUpdate();
         }
     }
@@ -34,35 +35,41 @@ public class TravelRepository {
         List<Travel> travels = new ArrayList<>();
         String sql = "SELECT " +
                 "t.id AS t_id, " +
-                "t.dest_x AS t_dest_x, " +
-                "t.dest_y AS t_dest_y, " +
-                "t.date AS t_date, " +
+                "t.createdAt AS t_createdAt, " +
+                "t.updatedAt AS t_updatedAt, " +
+                "t.x AS t_x, " +
+                "t.y AS t_y, " +
                 "t.distance AS t_distance, " +
                 "t.time AS t_time, " +
                 "t.cost AS t_cost, " +
                 "t.status AS t_status, " +
-                "pd.id AS pd_id, " +
-                "pd.name AS pd_name, " +
-                "pd.score AS pd_score, " +
-                "pd.x AS pd_x, " +
-                "pd.y AS pd_y, " +
-                "pt.id AS pt_id, " +
-                "pt.name AS pt_name, " +
-                "pt.score AS pt_score, " +
-                "pt.x AS pt_x, " +
-                "pt.y AS pt_y, " +
-                "traveler.phone AS pt_phone, " +
-                "car.id AS car_id, " +
-                "car.model AS car_model, " +
-                "car.color AS car_color, " +
-                "car.licensePlate AS car_licensePlate " +
+                "d.id AS d_id, " +
+                "d.createdAt AS d_createdAt, " +
+                "d.updatedAt AS d_updatedAt, " +
+                "d.name AS d_name, " +
+                "d.phone AS d_phone, " +
+                "d.x AS d_x, " +
+                "d.y AS d_y, " +
+                "d.score AS d_score, " +
+                "c.id AS c_id, " +
+                "c.createdAt AS c_createdAt, " +
+                "c.updatedAt AS c_updatedAt, " +
+                "c.name AS c_name, " +
+                "c.color AS c_color, " +
+                "c.licensePlate AS c_licensePlate " +
+                "u.id AS u_id, " +
+                "u.createdAt AS u_createdAt, " +
+                "u.updatedAt AS u_updatedAt, " +
+                "u.name AS u_name, " +
+                "u.phone AS u_phone, " +
+                "u.x AS u_x, " +
+                "u.y AS u_y, " +
+                "u.score AS u_score, " +
                 "FROM travel t " +
-                "JOIN person pd ON t.driver_id = pd.id " +
-                "JOIN driver ON pd.id = driver.id " +
-                "JOIN person pt ON t.traveler_id = pt.id " +
-                "JOIN traveler ON pt.id = traveler.id " +
-                "JOIN car ON car.id = driver.car_id " +
-                "ORDER BY t_date DESC";
+                "JOIN driver d ON t.driver_id = d.id " +
+                "JOIN car c ON c.id = d.car_id " +
+                "JOIN user u ON t.user_id = u.id " +
+                "ORDER BY t_createdAt DESC";
 
         try (
                 Connection connection = DBConnection.getConnection();
@@ -71,32 +78,41 @@ public class TravelRepository {
         ) {
             while (rs.next()) {
                 Car car = new Car(
-                        rs.getString("car_id"),
-                        rs.getString("car_model"),
-                        rs.getString("car_color"),
-                        rs.getString("car_licensePlate"));
+                        rs.getString("c_id"),
+                        rs.getString("c_createdAt"),
+                        rs.getString("c_updatedAt"),
+                        rs.getString("c_name"),
+                        rs.getString("c_color"),
+                        rs.getString("c_licensePlate"));
 
                 Driver driver = new Driver(
-                        rs.getString("pd_id"),
-                        rs.getString("pd_name"),
-                        rs.getDouble("pd_score"),
-                        car);
-                driver.setX(rs.getInt("pd_x"));
-                driver.setY(rs.getInt("pd_y"));
+                        car,
+                        rs.getString("d_id"),
+                        rs.getString("d_createdAt"),
+                        rs.getString("d_updatedAt"),
+                        rs.getString("d_name"),
+                        rs.getString("d_phone"),
+                        rs.getInt("d_x"),
+                        rs.getInt("d_y"),
+                        rs.getDouble("d_score"));
 
-                Traveler traveler = new Traveler(
-                        rs.getString("pt_id"),
-                        rs.getString("pt_name"),
-                        rs.getString("pt_phone"),
-                        rs.getDouble("pt_score"));
-                traveler.setX(rs.getInt("pt_x"));
-                traveler.setY(rs.getInt("pt_y"));
+                User user = new User(
+                        rs.getString("u_id"),
+                        rs.getString("u_createdAt"),
+                        rs.getString("u_updatedAt"),
+                        rs.getString("u_name"),
+                        rs.getString("u_phone"),
+                        rs.getInt("u_x"),
+                        rs.getInt("u_y"),
+                        rs.getDouble("u_score"));
 
                 Travel travel = new Travel(
                         rs.getString("t_id"),
-                        driver, traveler,
-                        new int[]{rs.getInt("t_dest_x"), rs.getInt("t_dest_y")},
-                        rs.getString("t_date"),
+                        rs.getString("t_createdAt"),
+                        rs.getString("t_updatedAt"),
+                        driver, user,
+                        rs.getInt("t_x"),
+                        rs.getInt("t_y"),
                         rs.getDouble("t_distance"),
                         rs.getInt("t_time"),
                         rs.getInt("t_cost"),
@@ -111,35 +127,41 @@ public class TravelRepository {
     public Travel getTravelById(String id) throws SQLException {
         String sql = "SELECT " +
                 "t.id AS t_id, " +
-                "t.dest_x AS t_dest_x, " +
-                "t.dest_y AS t_dest_y, " +
-                "t.date AS t_date, " +
+                "t.createdAt AS t_createdAt, " +
+                "t.updatedAt AS t_updatedAt, " +
+                "t.x AS t_x, " +
+                "t.y AS t_y, " +
                 "t.distance AS t_distance, " +
                 "t.time AS t_time, " +
                 "t.cost AS t_cost, " +
                 "t.status AS t_status, " +
-                "pd.id AS pd_id, " +
-                "pd.name AS pd_name, " +
-                "pd.score AS pd_score, " +
-                "pd.x AS pd_x, " +
-                "pd.y AS pd_y, " +
-                "pt.id AS pt_id, " +
-                "pt.name AS pt_name, " +
-                "pt.score AS pt_score, " +
-                "pt.x AS pt_x, " +
-                "pt.y AS pt_y, " +
-                "pt.phone AS pt_phone, " +
-                "car.id AS car_id, " +
-                "car.model AS car_model, " +
-                "car.color AS car_color, " +
-                "car.licensePlate AS car_licensePlate " +
+                "d.id AS d_id, " +
+                "d.createdAt AS d_createdAt, " +
+                "d.updatedAt AS d_updatedAt, " +
+                "d.name AS d_name, " +
+                "d.phone AS d_phone, " +
+                "d.x AS d_x, " +
+                "d.y AS d_y, " +
+                "d.score AS d_score, " +
+                "c.id AS c_id, " +
+                "c.createdAt AS c_createdAt, " +
+                "c.updatedAt AS c_updatedAt, " +
+                "c.name AS c_name, " +
+                "c.color AS c_color, " +
+                "c.licensePlate AS c_licensePlate " +
+                "u.id AS u_id, " +
+                "u.createdAt AS u_createdAt, " +
+                "u.updatedAt AS u_updatedAt, " +
+                "u.name AS u_name, " +
+                "u.phone AS u_phone, " +
+                "u.x AS u_x, " +
+                "u.y AS u_y, " +
+                "u.score AS u_score, " +
                 "FROM travel t " +
-                "JOIN person pd ON t.driver_id = pd.id " +
-                "JOIN driver ON pd.id = driver.id " +
-                "JOIN person pt ON t.traveler_id = pt.id " +
-                "JOIN traveler ON pt.id = traveler.id " +
-                "JOIN car ON car.id = driver.car_id " +
-                "WHERE t.id = ?";
+                "JOIN driver d ON t.driver_id = d.id " +
+                "JOIN car c ON c.id = d.car_id " +
+                "JOIN user u ON t.user_id = u.id " +
+                "WHERE t_id = ?";
 
         try (
                 Connection connection = DBConnection.getConnection();
@@ -149,32 +171,41 @@ public class TravelRepository {
             try (ResultSet rs = preStatement.executeQuery()) {
                 if (rs.next()) {
                     Car car = new Car(
-                            rs.getString("car_id"),
-                            rs.getString("car_model"),
-                            rs.getString("car_color"),
-                            rs.getString("car_licensePlate"));
+                            rs.getString("c_id"),
+                            rs.getString("c_createdAt"),
+                            rs.getString("c_updatedAt"),
+                            rs.getString("c_name"),
+                            rs.getString("c_color"),
+                            rs.getString("c_licensePlate"));
 
                     Driver driver = new Driver(
-                            rs.getString("pd_id"),
-                            rs.getString("pd_name"),
-                            rs.getDouble("pd_score"),
-                            car);
-                    driver.setX(rs.getInt("pd_x"));
-                    driver.setY(rs.getInt("pd_y"));
+                            car,
+                            rs.getString("d_id"),
+                            rs.getString("d_createdAt"),
+                            rs.getString("d_updatedAt"),
+                            rs.getString("d_name"),
+                            rs.getString("d_phone"),
+                            rs.getInt("d_x"),
+                            rs.getInt("d_y"),
+                            rs.getDouble("d_score"));
 
-                    Traveler traveler = new Traveler(
-                            rs.getString("pt_id"),
-                            rs.getString("pt_name"),
-                            rs.getString("pt_phone"),
-                            rs.getDouble("pt_score"));
-                    traveler.setX(rs.getInt("pt_x"));
-                    traveler.setY(rs.getInt("pt_y"));
+                    User user = new User(
+                            rs.getString("u_id"),
+                            rs.getString("u_createdAt"),
+                            rs.getString("u_updatedAt"),
+                            rs.getString("u_name"),
+                            rs.getString("u_phone"),
+                            rs.getInt("u_x"),
+                            rs.getInt("u_y"),
+                            rs.getDouble("u_score"));
 
                     return new Travel(
                             rs.getString("t_id"),
-                            driver, traveler,
-                            new int[]{rs.getInt("t_dest_x"), rs.getInt("t_dest_y")},
-                            rs.getString("t_date"),
+                            rs.getString("t_createdAt"),
+                            rs.getString("t_updatedAt"),
+                            driver, user,
+                            rs.getInt("t_x"),
+                            rs.getInt("t_y"),
                             rs.getDouble("t_distance"),
                             rs.getInt("t_time"),
                             rs.getInt("t_cost"),
@@ -188,11 +219,11 @@ public class TravelRepository {
     public void updateTravel(Travel travel) throws SQLException {
         String sql = "UPDATE travel " +
                 "SET " +
+                "updatedAt = ?, " +
                 "driver_id = ?, " +
-                "traveler_id = ?, " +
-                "date = ?, " +
-                "dest_x = ?, " +
-                "dest_y = ?, " +
+                "user_id = ?, " +
+                "x = ?, " +
+                "y = ?, " +
                 "distance = ?, " +
                 "time = ?, " +
                 "cost = ?, " +
@@ -203,11 +234,11 @@ public class TravelRepository {
                 Connection connection = DBConnection.getConnection();
                 PreparedStatement prepStmt = connection.prepareStatement(sql)
         ) {
-            prepStmt.setString(1, travel.getDriver().getId());
-            prepStmt.setString(2, travel.getTraveler().getId());
-            prepStmt.setString(3, travel.getDate());
-            prepStmt.setInt(4, travel.getDestination()[0]);
-            prepStmt.setInt(5, travel.getDestination()[1]);
+            prepStmt.setString(1, travel.getUpdatedAt().toString());
+            prepStmt.setString(2, travel.getDriver().getId());
+            prepStmt.setString(3, travel.getUser().getId());
+            prepStmt.setInt(4, travel.getX());
+            prepStmt.setInt(5, travel.getY());
             prepStmt.setDouble(6, travel.getDistance());
             prepStmt.setInt(7, travel.getTime());
             prepStmt.setInt(8, travel.getCost());
