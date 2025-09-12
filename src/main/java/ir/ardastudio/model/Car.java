54a67b435e610c1585381588a65887e8bdfc5069
Aref Daei <aref.daei@outlook.com>
2025-09-12 12:50:02 +0330

    Rename model field to name and add onUpdate calls to setters

--- src/main/java/ir/ardastudio/model/Car.java
+++ src/main/java/ir/ardastudio/model/Car.java
@@ -4,34 +4,35 @@ import java.time.LocalDateTime;
 
 public class Car extends Model {
 
-	private String model;
+	private String name;
 	private String color;
 	private String licensePlate;
 
-	public Car(String model, String color, String licensePlate) {
+	public Car(String name, String color, String licensePlate) {
         onId();
         onCreate();
 
-		this.model = model;
+		this.name = name;
 		this.color = color;
 		this.licensePlate = licensePlate;
 	}
 
     public Car(String id, String createdAt, String updatedAt,
-               String model, String color, String licensePlate) {
+               String name, String color, String licensePlate) {
         super(id, LocalDateTime.parse(createdAt), LocalDateTime.parse(updatedAt));
 
-        this.model = model;
+        this.name = name;
         this.color = color;
         this.licensePlate = licensePlate;
     }
 
-	public String getModel() {
-		return model;
+	public String getName() {
+		return name;
 	}
 
-	public void setModel(String model) {
-		this.model = model;
+	public void setName(String name) {
+        onUpdate();
+		this.name = name;
 	}
 
 	public String getColor() {
@@ -39,6 +40,7 @@ public class Car extends Model {
 	}
 
 	public void setColor(String color) {
+        onUpdate();
 		this.color = color;
 	}
 
@@ -47,12 +49,13 @@ public class Car extends Model {
 	}
 
 	public void setLicensePlate(String licensePlate) {
+        onUpdate();
 		this.licensePlate = licensePlate;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%s %s, %s IR", getModel(), getColor(), getLicensePlate());
+		return String.format("%s %s, %s IR", getName(), getColor(), getLicensePlate());
 	}
 
 }
